8ec183dcb694c30faf8d92465df7691230168342
skydreamer21 <skydreamer210@gmail.com>
2022-12-14 21:14:53 +0900

    feat(DomainErrorMessage): 에러메세지 추가

--- src/main/java/subway/domain/constant/DomainErrorMessage.java
+++ src/main/java/subway/domain/constant/DomainErrorMessage.java
@@ -1,7 +1,10 @@
 package subway.domain.constant;
 
 public enum DomainErrorMessage {
-    DUPLICATE_STATION("이미 등록된 역 이름입니다.");
+    INVALID_NAME_LENGTH("역이름은 최소 " + StationProperties.MIN_NAME_LENGTH.getProperty()
+            + "글자 이상이어야 합니다."),
+    DUPLICATE_STATION("이미 등록된 역 이름입니다."),
+    NOT_EXIST_STATION("해당 역은 존재하지 않습니다.");
 
     private static final String PREFIX = "[ERROR] ";
     private final String message;
