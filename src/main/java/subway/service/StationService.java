75954608ccdce9f563b9d3f71b7222d771bd2774
skydreamer21 <skydreamer210@gmail.com>
2022-12-14 21:49:44 +0900

    feat(StationService): 역 삭제 및 조회 서비스 기능 구현

--- src/main/java/subway/service/StationService.java
+++ src/main/java/subway/service/StationService.java
@@ -3,6 +3,8 @@ package subway.service;
 import subway.domain.Station;
 import subway.repository.StationRepository;
 
+import java.util.Set;
+
 public class StationService {
     private static class InstanceHolder {
         private static final StationService INSTANCE = new StationService();
@@ -18,5 +20,13 @@ public class StationService {
 
     public void registerStation(String name) {
         stationRepository.addStation(new Station(name));
+    }
+
+    public void removeStation(String name) {
+        stationRepository.deleteStation(name);
+    }
+
+    public Set<String> loadAllStations() {
+        return stationRepository.findAllStations();
     }
 }
