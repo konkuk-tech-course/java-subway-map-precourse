02ac5a6c8d3e10b194266b5367f8bdfa8c2620d9
skydreamer21 <skydreamer210@gmail.com>
2022-12-14 21:44:32 +0900

    feat(StationService): 새로운 역 등록 기능

--- src/main/java/subway/service/StationService.java
+++ src/main/java/subway/service/StationService.java
@@ -1,15 +1,22 @@
 package subway.service;
 
-import subway.domain.StationRepository;
+import subway.domain.Station;
+import subway.repository.StationRepository;
 
 public class StationService {
     private static class InstanceHolder {
         private static final StationService INSTANCE = new StationService();
     }
 
+    private final StationRepository stationRepository = StationRepository.getInstance();
+
     private StationService(){}
 
     public StationService getInstance() {
         return StationService.InstanceHolder.INSTANCE;
+    }
+
+    public void registerStation(String name) {
+        stationRepository.addStation(new Station(name));
     }
 }
