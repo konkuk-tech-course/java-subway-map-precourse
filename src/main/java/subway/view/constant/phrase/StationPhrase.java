f4333ceb98899abf96cc3eb0d3a010e4cb6835df
skydreamer21 <skydreamer210@gmail.com>
2022-12-15 17:24:23 +0900

    feat(StationPhrase): 역 관련 INFO 문구 추가

--- src/main/java/subway/view/constant/phrase/StationPhrase.java
+++ src/main/java/subway/view/constant/phrase/StationPhrase.java
@@ -3,7 +3,9 @@ package subway.view.constant.phrase;
 public enum StationPhrase {
     TITLE("역 관리 화면"),
     REGISTER("등록할 역 이름을 입력하세요."),
-    DELETE("삭제할 역 이름을 입력하세요.");
+    REGISTER_INFO("지하철 역이 등록되었습니다."),
+    DELETE("삭제할 역 이름을 입력하세요."),
+    DELETE_INFO("지하철 역이 삭제되었습니다.");
 
     private final String phrase;
 
