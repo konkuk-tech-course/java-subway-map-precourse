f61541029286dcd771a7ea7ecdf2cc52e930a1a6
juno-junho <ssmm0205@naver.com>
2022-12-15 14:42:33 +0900

    feat(domain): default station 생성

--- src/main/java/subway/domain/StationRepository.java
+++ src/main/java/subway/domain/StationRepository.java
@@ -6,7 +6,21 @@ import java.util.List;
 import java.util.Objects;
 
 public class StationRepository {
-    private static final List<Station> stations = new ArrayList<>();
+    private static final List<Station> stations;
+
+    static {
+        List<Station> defaultStation = List.of(
+                new Station("교대역"),
+                new Station("강남역"),
+                new Station("역삼역"),
+                new Station("남부터미널역"),
+                new Station("양재역"),
+                new Station("양재시민의숲역"),
+                new Station("매봉역")
+        );
+        stations = new ArrayList<>(defaultStation);
+    }
+
 
     public static List<Station> stations() {
         return Collections.unmodifiableList(stations);
@@ -18,5 +32,9 @@ public class StationRepository {
 
     public static boolean deleteStation(String name) {
         return stations.removeIf(station -> Objects.equals(station.getName(), name));
+    }
+
+    public StationRepository() {
+
     }
 }
