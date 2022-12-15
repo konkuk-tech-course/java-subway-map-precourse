be90d4e092c532644c84be64bbd819cae51baa7e
skydreamer21 <skydreamer210@gmail.com>
2022-12-15 15:56:00 +0900

    feat(OutputView): 싱글톤 틀 구현

---
+++ src/main/java/subway/view/OutputView.java
@@ -0,0 +1,13 @@
+package subway.view;
+
+public class OutputView {
+    private static class InstanceHolder {
+        private static final OutputView INSTANCE = new OutputView();
+    }
+
+    private OutputView(){}
+
+    public static OutputView getInstance() {
+        return OutputView.InstanceHolder.INSTANCE;
+    }
+}
