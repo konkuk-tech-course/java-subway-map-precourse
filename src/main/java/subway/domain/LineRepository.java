ecac923adbee7a7f7f9bf220f680156fffdedbfe
juno-junho <ssmm0205@naver.com>
2022-12-15 15:35:21 +0900

    feat(domain): LineRepository 통해 기본 역 생성

--- src/main/java/subway/domain/LineRepository.java
+++ src/main/java/subway/domain/LineRepository.java
@@ -10,9 +10,19 @@ public class LineRepository {
 
     static {
         List<Line> defaultLine = List.of(
-                new Line("2호선"),
-                new Line("3호선"),
-                new Line("신분당선")
+                new Line("2호선", List.of(
+                        new Station("교대역"),
+                        new Station("강남역"),
+                        new Station("역삼역"))),
+                new Line("3호선", List.of(
+                        new Station("교대역"),
+                        new Station("남부터미널역"),
+                        new Station("양재역"),
+                        new Station("매봉역"))),
+                new Line("신분당선", List.of(
+                        new Station("강남역"),
+                        new Station("양재역"),
+                        new Station("양재시민의숲역")))
         );
         lines = new ArrayList<>(defaultLine);
     }
@@ -27,5 +37,9 @@ public class LineRepository {
 
     public static boolean deleteLineByName(String name) {
         return lines.removeIf(line -> Objects.equals(line.getName(), name));
+    }
+
+    public static List<Line> getLines() {
+        return lines;
     }
 }
