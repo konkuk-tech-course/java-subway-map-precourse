26761bdcf76974e49198bb0190d758543f91ac8c
skydreamer21 <skydreamer210@gmail.com>
2022-12-15 19:51:16 +0900

    feat(SectionPhrase): 구간 기능 성공 문구 추가

--- src/main/java/subway/view/constant/phrase/SectionPhrase.java
+++ src/main/java/subway/view/constant/phrase/SectionPhrase.java
@@ -6,7 +6,9 @@ public enum SectionPhrase {
     REGISTER_STATION_INPUT("역이름을 입력하세요."),
     REGISTER_ORDER_INPUT("순서를 입력하세요."),
     DELETE_LINE_INPUT("삭제할 구간의 노선을 입력하세요."),
-    DELETE_STATION_INPUT("삭제할 구간의 역을 입력하세요.");
+    DELETE_STATION_INPUT("삭제할 구간의 역을 입력하세요."),
+    REGISTER_INFO("구간이 등록되었습니다."),
+    DELETE_INFO("구간이 삭제되었습니다.");
 
     private final String phrase;
 
