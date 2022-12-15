0ef559c0927bf60a6b8481b9be53e2cb78967b00
skydreamer21 <skydreamer210@gmail.com>
2022-12-15 17:50:09 +0900

    feat(LineRepository): 호선 등록 서비스 기능 구현

--- src/main/java/subway/service/LineService.java
+++ src/main/java/subway/service/LineService.java
@@ -1,13 +1,29 @@
 package subway.service;
 
+import subway.domain.Line;
+import subway.domain.Station;
+import subway.repository.LineRepository;
+
+import java.util.List;
+
 public class LineService {
     private static class InstanceHolder {
         private static final LineService INSTANCE = new LineService();
     }
+
+    private final LineRepository lineRepository = LineRepository.getInstance();
 
     private LineService(){}
 
     public static LineService getInstance() {
         return LineService.InstanceHolder.INSTANCE;
     }
+
+    public void registerLine(String name, String startStationName, String endStationName) {
+        Line line = new Line(name);
+        line.addStation(new Station(startStationName));
+        line.addStation(new Station(endStationName));
+        lineRepository.addLine(line);
+    }
+
 }
