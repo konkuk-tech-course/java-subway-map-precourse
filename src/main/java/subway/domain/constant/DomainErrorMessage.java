8a882ecb054161457e61dd47da774976854b2eee
skydreamer21 <skydreamer210@gmail.com>
2022-12-15 17:09:15 +0900

    fix(DomainErrorMessage): 에러 접두사 삭제

--- src/main/java/subway/domain/constant/DomainErrorMessage.java
+++ src/main/java/subway/domain/constant/DomainErrorMessage.java
@@ -6,7 +6,6 @@ public enum DomainErrorMessage {
     DUPLICATE_STATION("이미 등록된 역 이름입니다."),
     NOT_EXIST_STATION("해당 역은 존재하지 않습니다.");
 
-    private static final String PREFIX = "[ERROR] ";
     private final String message;
 
     private DomainErrorMessage(String message) {
@@ -14,6 +13,6 @@ public enum DomainErrorMessage {
     }
 
     public String get() {
-        return PREFIX + message;
+        return message;
     }
 }
