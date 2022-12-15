210ef1dd795a3287570361cd488fe9bba51222e3
skydreamer21 <skydreamer210@gmail.com>
2022-12-15 15:45:15 +0900

    refactor(phrase/): 공통 문구 분리

--- src/main/java/subway/view/constant/phrase/CommonPhrase.java
+++ src/main/java/subway/view/constant/phrase/CommonPhrase.java
@@ -1,11 +1,11 @@
 package subway.view.constant.phrase;
 
-public enum MainPhrase {
-    TITLE("메인 화면");
+public enum CommonPhrase {
+    OPTION_INPUT("원하는 기능을 선택하세요.");
 
     private final String phrase;
 
-    MainPhrase(String phrase) {
+    CommonPhrase(String phrase) {
         this.phrase = phrase;
     }
 
