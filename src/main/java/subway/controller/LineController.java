0cf60aeb4334558c84645396d9101ebdc938c7fd
skydreamer21 <skydreamer210@gmail.com>
2022-12-15 19:40:11 +0900

    feat(LineController): 호선 관련 서비스 기능 구현

---
+++ src/main/java/subway/controller/LineController.java
@@ -0,0 +1,73 @@
+package subway.controller;
+
+import subway.service.LineService;
+import subway.view.InputView;
+import subway.view.OutputView;
+import subway.view.constant.option.LineOptions;
+import subway.view.constant.phrase.LinePhrase;
+import subway.view.constant.phrase.StationPhrase;
+
+import java.util.List;
+import java.util.Set;
+
+public class LineController {
+    private static class InstanceHolder {
+        private static final LineController INSTANCE = new LineController();
+    }
+
+    private InputView inputView = InputView.getInstance();
+    private OutputView outputView = OutputView.getInstance();
+    private LineService lineService = LineService.getInstance();
+
+    private LineController(){}
+
+    public static LineController getInstance() {
+        return LineController.InstanceHolder.INSTANCE;
+    }
+
+    public void operate() {
+        String option = Requester.requestStringInput(inputView::readStationOption);
+        handleOption(option);
+    }
+
+    private void handleOption(String option) {
+        if (option.equals(LineOptions.REGISTER.getOption())) {
+            registerLine();
+        }
+        if (option.equals(LineOptions.DELETE.getOption())) {
+            removeLine();
+        }
+        if (option.equals(LineOptions.FIND_ALL.getOption())) {
+            showAllLines();
+        }
+    }
+
+    private void registerLine() {
+        String lineName = Requester.requestStringInput(inputView::readStationNameToBeRegistered);
+        String startStationName = Requester.requestStringInput(inputView::readStartStationName);
+        String endStationName = Requester.requestStringInput(inputView::readEndStationName);
+        try {
+            lineService.registerLine(lineName, startStationName, endStationName);
+        } catch (IllegalArgumentException e) {
+            outputView.printErrorMessage(e.getMessage());
+            registerLine();
+        }
+        outputView.printInfoPhrase(LinePhrase.REGISTER_INFO.get());
+    }
+
+    private void removeLine() {
+        String lineName = Requester.requestStringInput(inputView::readLineNameToBeDeleted);
+        try {
+            lineService.removeLine(lineName);
+        } catch (IllegalArgumentException e) {
+            outputView.printErrorMessage(e.getMessage());
+            removeLine();
+        }
+        outputView.printInfoPhrase(LinePhrase.DELETE_INFO.get());
+    }
+
+    private void showAllLines() {
+        List<String> lines = lineService.loadAllLines();
+        outputView.printLines(lines);
+    }
+}
