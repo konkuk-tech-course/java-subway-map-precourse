ca25b41e8d6b5219d07a861c4e4594ea7db39878
skydreamer21 <skydreamer210@gmail.com>
2022-12-15 17:35:05 +0900

    feat(Line): 역 추가 기능 구현

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -1,12 +1,12 @@
 package subway.domain;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Line {
     private String name;
-    private List<Station> stations = new ArrayList<>();
+    private List<Station> stations = new LinkedList<>();
 
     public Line(String name) {
         this.name = name;
@@ -14,6 +14,14 @@ public class Line {
 
     public String getName() {
         return name;
+    }
+
+    public void addStation(int order, Station station) {
+        stations.add(orderToIndex(order), station);
+    }
+
+    private int orderToIndex(int order) {
+        return --order;
     }
 
     public List<String> findAllStations() {
