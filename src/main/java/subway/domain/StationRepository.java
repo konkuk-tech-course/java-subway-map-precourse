3605d9a35a7606be457f7b80ae67d54c20069d5c
juno-junho <ssmm0205@naver.com>
2022-12-15 15:36:10 +0900

    feat(domain): StationRepository의 LineRepository에 의존한 역 관리 지정

--- src/main/java/subway/domain/StationRepository.java
+++ src/main/java/subway/domain/StationRepository.java
@@ -1,26 +1,18 @@
 package subway.domain;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class StationRepository {
     private static final List<Station> stations;
 
     static {
-        List<Station> defaultStation = List.of(
-                new Station("교대역"),
-                new Station("강남역"),
-                new Station("역삼역"),
-                new Station("남부터미널역"),
-                new Station("양재역"),
-                new Station("양재시민의숲역"),
-                new Station("매봉역")
-        );
-        stations = new ArrayList<>(defaultStation);
+        stations = LineRepository.getLines().stream()
+                .flatMap(line -> line.getStations().stream())
+                .distinct().collect(Collectors.toList());
     }
-
 
     public static List<Station> stations() {
         return Collections.unmodifiableList(stations);
@@ -34,7 +26,7 @@ public class StationRepository {
         return stations.removeIf(station -> Objects.equals(station.getName(), name));
     }
 
-    public StationRepository() {
-
+    public static List<Station> getStations() {
+        return stations;
     }
 }
