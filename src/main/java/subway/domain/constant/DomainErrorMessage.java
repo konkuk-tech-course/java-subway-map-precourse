1365153ba3033f62475acf701eb428deb59197a6
skydreamer21 <skydreamer210@gmail.com>
2022-12-15 19:29:11 +0900

    feat(DomainErrorMessage): 중복된 호선 에러메세지 추가

--- src/main/java/subway/domain/constant/DomainErrorMessage.java
+++ src/main/java/subway/domain/constant/DomainErrorMessage.java
@@ -5,7 +5,8 @@ public enum DomainErrorMessage {
             + "글자 이상이어야 합니다."),
     DUPLICATE_STATION("이미 등록된 역 이름입니다."),
     NOT_EXIST_STATION("해당 역은 존재하지 않습니다."),
-    INVALID_ORDER("잘못된 순서입니다.");
+    INVALID_ORDER("잘못된 순서입니다."),
+    DUPLICATE_LINE("이미 등록된 호선입니다.");
 
     private final String message;
 
