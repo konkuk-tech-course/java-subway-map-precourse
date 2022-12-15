962a009fc5bdf9c346687b3fbb8ed3170979823f
HaODay <ycis5013@naver.com>
2022-12-15 14:22:06 +0900

    feat(OutputView): 메인 메뉴 출력하는 기능 구현

---
+++ src/main/java/subway/view/OutputView.java
@@ -0,0 +1,11 @@
+package subway.view;
+
+import static subway.util.MenuMessage.MAIN_MENU;
+
+public class OutputView {
+
+    public void printMainMenu(){
+        System.out.println(MAIN_MENU);
+
+    }
+}
