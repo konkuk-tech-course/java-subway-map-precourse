c397515e8da4ff54f63ad7663a96f0ca55669e73
skydreamer21 <skydreamer210@gmail.com>
2022-12-15 16:46:04 +0900

    fix(ValidationRegex): 잘못된 옵션 수정

--- src/main/java/subway/view/constant/ValidationRegex.java
+++ src/main/java/subway/view/constant/ValidationRegex.java
@@ -1,13 +1,14 @@
 package subway.view.constant;
 
+import subway.view.constant.option.Options;
+
 public enum ValidationRegex {
     EMPTY("^$"),
     NUMBER("^[0-9]+$"),
-    MAIN_OPTION("^[1-4|Q]{1}$"),
-    MAIN_MENU("^[1-4|Q]{1}$"),
-    STATION_MENU("^[1-3|Q]{1}$"),
-    LINE_MENU("^[1-3|Q]{1}$"),
-    SECTION_MENU("^[1-2|Q]{1}$");
+    MAIN_MENU("^[1-4|" + Options.QUIT.get() + "]{1}$"),
+    STATION_MENU("^[1-3|" + Options.BACK.get() + "]{1}$"),
+    LINE_MENU("^[1-3|" + Options.BACK.get() + "]{1}$"),
+    SECTION_MENU("^[1-2|" + Options.BACK.get() + "]{1}$");
 
     private final String regex;
 
