d0f2843be5f2bbeae5617ac066f8733da8729bf2
skydreamer21 <skydreamer210@gmail.com>
2022-12-15 20:33:45 +0900

    feat(Application): 메인 메서드 구현

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -1,10 +1,31 @@
 package subway;
 
+import subway.controller.MainController;
+import subway.domain.Station;
+import subway.repository.LineRepository;
+import subway.repository.StationRepository;
+import subway.view.InputView;
+
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO: 프로그램 구현
+        init();
+        InputView inputView = InputView.getInstance();
+        MainController mainController = MainController.getInstance();
+        mainController.operate();
+    }
+
+    public static void init() {
+        StationRepository stationRepository = StationRepository.getInstance();
+        stationRepository.addStation(new Station("교대역"));
+        stationRepository.addStation(new Station("강남역"));
+        stationRepository.addStation(new Station("역삼역"));
+        stationRepository.addStation(new Station("남부터미널역"));
+        stationRepository.addStation(new Station("양재역"));
+        stationRepository.addStation(new Station("양재시민의숲역"));
+        stationRepository.addStation(new Station("매봉역"));
     }
 }
