e55cb953ddfccfbb03f631019627ac01abf9ce45
skydreamer21 <skydreamer210@gmail.com>
2022-12-15 16:14:52 +0900

    refactor(Requester): 문자열 반복 입력 함수 이외 삭제

--- src/main/java/subway/controller/Requester.java
+++ src/main/java/subway/controller/Requester.java
@@ -7,21 +7,12 @@ import java.util.function.Supplier;
 public class Requester {
     private static final OutputView outputView = OutputView.getInstance();
 
-    public static int requestOptionInput(Supplier<Integer> supplier) {
+    public static String requestStringInput(Supplier<String> supplier) {
         try {
             return supplier.get();
         } catch (IllegalArgumentException e) {
             outputView.printErrorMessage(e.getMessage());
-            return requestOptionInput(supplier);
-        }
-    }
-
-    public static String requestNameInput(Supplier<String> supplier) {
-        try {
-            return supplier.get();
-        } catch (IllegalArgumentException e) {
-            outputView.printErrorMessage(e.getMessage());
-            return requestNameInput(supplier);
+            return requestStringInput(supplier);
         }
     }
 }
