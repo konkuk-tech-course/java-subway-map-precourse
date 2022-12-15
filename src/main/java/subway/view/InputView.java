81ccf251e7112cf338d299246d4be22fb5a94892
skydreamer21 <skydreamer210@gmail.com>
2022-12-15 15:40:26 +0900

    feat(InputView): 메인 메뉴의 옵션 입력 기능 구현

--- src/main/java/subway/view/InputView.java
+++ src/main/java/subway/view/InputView.java
@@ -1,9 +1,11 @@
 package subway.view;
 
+import subway.util.Reader;
 import subway.view.constant.Prefix;
 import subway.view.constant.menu.Menu;
 import subway.view.constant.phrase.MainPhrase;
 
+import java.io.Console;
 import java.util.Arrays;
 
 public class InputView {
@@ -17,7 +19,17 @@ public class InputView {
         return InputView.InstanceHolder.INSTANCE;
     }
 
-    
+    public int readMainOption() {
+        printPhrase(MainPhrase.TITLE.get());
+        String input = Reader.readLine().trim();
+        ViewValidator.validateMainOption(input);
+        return Integer.parseInt(input);
+    }
+
+    public void printPhrase(String phrase) {
+        System.out.println(Prefix.PHRASE.get() + phrase);
+    }
+
     public <T extends Menu> void printMenu(Class<T> menu) {
         Arrays.stream(menu.getEnumConstants())
                 .map(Menu::getMenu)
