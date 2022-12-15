2fa2e61c1aba7445d8c7e9ba25c16f47a402ccc6
skydreamer21 <skydreamer210@gmail.com>
2022-12-15 19:33:13 +0900

    feat(LineService): 이름 유효성 검증
    [세부사항]
    - 검증 사항
    1. 호선 이름이 이미 있는지 확인
    2. 존재하지 않는 역인지 확인

--- src/main/java/subway/service/LineService.java
+++ src/main/java/subway/service/LineService.java
@@ -2,7 +2,9 @@ package subway.service;
 
 import subway.domain.Line;
 import subway.domain.Station;
+import subway.domain.constant.DomainErrorMessage;
 import subway.repository.LineRepository;
+import subway.repository.StationRepository;
 
 import java.util.List;
 
@@ -11,6 +13,7 @@ public class LineService {
         private static final LineService INSTANCE = new LineService();
     }
 
+    private final StationRepository stationRepository = StationRepository.getInstance();
     private final LineRepository lineRepository = LineRepository.getInstance();
 
     private LineService(){}
@@ -20,10 +23,29 @@ public class LineService {
     }
 
     public void registerLine(String name, String startStationName, String endStationName) {
+        validateNewLine(name, startStationName, endStationName);
         Line line = new Line(name);
         line.addStation(new Station(startStationName));
         line.addStation(new Station(endStationName));
         lineRepository.addLine(line);
+    }
+
+    private void validateNewLine(String name, String startStationName, String endStationName) {
+        validateLineName(name);
+        validateStation(startStationName);
+        validateStation(endStationName);
+    }
+
+    private void validateLineName(String name) {
+        if (lineRepository.hasLine(name)) {
+            throw new IllegalArgumentException(DomainErrorMessage.DUPLICATE_LINE.get());
+        }
+    }
+
+    private void validateStation(String stationName) {
+        if (!stationRepository.hasStation(stationName)) {
+            throw new IllegalArgumentException(DomainErrorMessage.NOT_EXIST_STATION.get());
+        }
     }
 
     public void removeLine(String name) {
