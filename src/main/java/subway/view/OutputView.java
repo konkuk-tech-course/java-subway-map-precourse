3f338fbc298cf30d3b2870cd2629f3f9a9ed2929
HaODay <ycis5013@naver.com>
2022-12-15 14:24:51 +0900

    refactor(OutputView): 상수명 변경

--- src/main/java/subway/view/OutputView.java
+++ src/main/java/subway/view/OutputView.java
@@ -1,11 +1,11 @@
 package subway.view;
 
-import static subway.util.MenuMessage.MAIN_MENU;
+import static subway.util.PageMessage.MAIN_PAGE;
 
 public class OutputView {
 
     public void printMainMenu(){
-        System.out.println(MAIN_MENU);
+        System.out.println(MAIN_PAGE);
 
     }
 }
