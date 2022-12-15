bef3ea72db314da587e81c7fabb23ee4f92e8aed
skydreamer21 <skydreamer210@gmail.com>
2022-12-15 17:31:55 +0900

    feat(DomainErrorMessage): 잘못된 순서 에러메세지 추가

--- src/main/java/subway/domain/constant/DomainErrorMessage.java
+++ src/main/java/subway/domain/constant/DomainErrorMessage.java
@@ -4,7 +4,8 @@ public enum DomainErrorMessage {
     INVALID_NAME_LENGTH("역이름은 최소 " + StationProperties.MIN_NAME_LENGTH.getProperty()
             + "글자 이상이어야 합니다."),
     DUPLICATE_STATION("이미 등록된 역 이름입니다."),
-    NOT_EXIST_STATION("해당 역은 존재하지 않습니다.");
+    NOT_EXIST_STATION("해당 역은 존재하지 않습니다."),
+    INVALID_ORDER("잘못된 순서입니다.");
 
     private final String message;
 
