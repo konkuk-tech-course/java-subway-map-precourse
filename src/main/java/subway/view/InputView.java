a080846f73b2f3a4eb02dba009f606f0fea9561c
juno-junho <ssmm0205@naver.com>
2022-12-15 16:37:36 +0900

    feat(InputView): 역과 노선에 대한 input 받는 기능 구현

---
+++ src/main/java/subway/view/InputView.java
@@ -0,0 +1,79 @@
+package subway.view;
+
+import subway.util.Validator;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    private final Scanner scanner;
+    private final Validator validator;
+
+    public InputView() {
+        this.scanner = new Scanner(System.in);
+        this.validator = new Validator();
+    }
+
+    public String readFunction() {
+        System.out.println("## 원하는 기능을 선택하세요.");
+        String input = scanner.nextLine();
+        validator.validateWhenTotalOptionsAreFive(input);
+        return input;
+    }
+
+    public String readStationFunction(){
+        System.out.println("## 원하는 기능을 선택하세요.");
+        String input = scanner.nextLine();
+        validator.validateWhenTotalOptionsAreFour(input);
+        return input;
+    }
+
+    public String readStationToRegister(){
+        System.out.println("## 등록할 역 이름을 입력하세요.");
+        String input = scanner.nextLine();
+        // TODO : VALIDATE 사항추가 (중복 및 2글자 이상 VALIDATE)
+        return input;
+    }
+
+    public String readStationToRemove() {
+        System.out.println("## 삭제할 역 이름을 입력하세요.");
+        String stationToRemove = scanner.nextLine();
+        return stationToRemove;
+    }
+
+    // ---------------노선관리----------------
+    public String readLineFunction() {
+        System.out.println("## 원하는 기능을 선택하세요.");
+        String input = scanner.nextLine();
+        validator.validateWhenTotalOptionsAreFour(input);
+        return input;
+    }
+
+    public String readLineToRegister(){
+        System.out.println("## 등록할 노선 이름을 입력하세요.");
+        String input = scanner.nextLine();
+        // TODO : VALIDATE 사항추가 (중복 및 2글자 이상 VALIDATE)
+        return input;
+    }
+
+    public String readUpStopStationName() {
+        System.out.println("## 등록할 노선의 상행 종점역 이름을 입력하세요.");
+        String upStopStationName = scanner.nextLine();
+        // TODO : VALIDATE
+        return upStopStationName;
+    }
+
+    public String readDownStopStationName() {
+        System.out.println("## 등록할 노선의 상행 종점역 이름을 입력하세요.");
+        String downStopStationName = scanner.nextLine();
+        // TODO : VALIDATE
+        return downStopStationName;
+    }
+
+    public String readLineToRemove() {
+        System.out.println("## 삭제할 노선 이름을 입력하세요.");
+        String lineToRemove = scanner.nextLine();
+
+        return lineToRemove;
+    }
+}
