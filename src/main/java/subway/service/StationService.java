6755de2326ca9c8bd0e7bcc8ed66da9ae79c167f
skydreamer21 <skydreamer210@gmail.com>
2022-12-16 16:17:26 +0900

    fix(StationService): 잘못된 에러메세지 수정

--- src/main/java/subway/service/StationService.java
+++ src/main/java/subway/service/StationService.java
@@ -35,7 +35,7 @@ public class StationService {
 
     public void validateNonExistStationName(String name) {
         if (stationRepository.hasStation(name)) {
-            throw new IllegalArgumentException(DomainErrorMessage.NOT_EXIST_STATION.get());
+            throw new IllegalArgumentException(DomainErrorMessage.DUPLICATE_STATION.get());
         }
     }
 
