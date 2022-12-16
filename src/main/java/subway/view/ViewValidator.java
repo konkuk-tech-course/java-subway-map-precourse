4065326ef7adf95552d28765221646ff54b2e2c0
skydreamer21 <skydreamer210@gmail.com>
2022-12-16 12:16:13 +0900

    feat(ViewValidator): 숫자 입력 유효성 검증 구현

--- src/main/java/subway/view/ViewValidator.java
+++ src/main/java/subway/view/ViewValidator.java
@@ -52,8 +52,15 @@ public class ViewValidator {
         }
     }
 
-    // TODO: 구간 옵션 입력 유효성 검증
-//    public static void
+    public static void validateNumberInput(String input) {
+        if (isEmpty(input)) {
+            throw new IllegalArgumentException(ViewErrorMessage.EMPTY.get());
+        }
+
+        if (!isNumber(input)) {
+            throw new IllegalArgumentException(ViewErrorMessage.NOT_NUMBER.get());
+        }
+    }
 
     private static boolean isEmpty(String input) {
         return Pattern.matches(ValidationRegex.EMPTY.get(), input);
