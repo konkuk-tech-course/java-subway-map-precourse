b1958f9b6691defa491d4ba07ef1ba6175c7a490
skydreamer21 <skydreamer210@gmail.com>
2022-12-16 13:00:37 +0900

    feat(Application): 호선 초기화 기능 구현

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -1,9 +1,11 @@
 package subway;
 
 import subway.controller.MainController;
+import subway.domain.Line;
 import subway.domain.Station;
 import subway.repository.LineRepository;
 import subway.repository.StationRepository;
+import subway.service.LineService;
 import subway.view.InputView;
 
 import java.util.Scanner;
@@ -12,13 +14,14 @@ public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO: 프로그램 구현
-        init();
+        initStations();
+        initLines();
         InputView inputView = InputView.getInstance();
         MainController mainController = MainController.getInstance();
         mainController.operate();
     }
 
-    public static void init() {
+    public static void initStations() {
         StationRepository stationRepository = StationRepository.getInstance();
         stationRepository.addStation(new Station("교대역"));
         stationRepository.addStation(new Station("강남역"));
@@ -27,5 +30,12 @@ public class Application {
         stationRepository.addStation(new Station("양재역"));
         stationRepository.addStation(new Station("양재시민의숲역"));
         stationRepository.addStation(new Station("매봉역"));
+    }
+
+    public static void initLines() {
+        LineService lineService = LineService.getInstance();
+        lineService.registerLine("2호선", "교대역", "역삼역");
+        lineService.registerLine("3호선", "교대역", "매봉역");
+        lineService.registerLine("신분당선", "강남역", "양재시민의숲역");
     }
 }
