1bdc7fb26a87411616cfc1b463aa0d78536c9637
skydreamer21 <skydreamer210@gmail.com>
2022-12-16 16:44:34 +0900

    feat(Application): 역 초기화 목록 추가

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -4,6 +4,7 @@ import subway.controller.MainController;
 import subway.domain.Station;
 import subway.repository.StationRepository;
 import subway.service.LineService;
+import subway.service.SectionService;
 
 public class Application {
     public static void main(String[] args) {
@@ -26,8 +27,12 @@ public class Application {
 
     public static void initLines() {
         LineService lineService = LineService.getInstance();
+        SectionService sectionService = SectionService.getInstance();
         lineService.registerLine("2호선", "교대역", "역삼역");
-        lineService.registerLine("3호선", "교대역", "매봉역");
-        lineService.registerLine("신분당선", "강남역", "양재시민의숲역");
+        lineService.registerLine("3호선", "교대역", "남부터미널역");
+        sectionService.registerSection("3호선", "양재역");
+        sectionService.registerSection("3호선", "매봉역");
+        lineService.registerLine("신분당선", "강남역", "양재역");
+        sectionService.registerSection("신분당선", "양재시민의숲역");
     }
 }
