f7a2a1d35d38fdd619e4ad29b0f0d31283e54146
skydreamer21 <skydreamer210@gmail.com>
2022-12-16 12:47:38 +0900

    feat(SectionController): 구간 입력 컨트롤러 기능 구현

--- src/main/java/subway/controller/SectionController.java
+++ src/main/java/subway/controller/SectionController.java
@@ -1,11 +1,17 @@
 package subway.controller;
 
+import subway.domain.constant.DomainErrorMessage;
 import subway.repository.LineRepository;
 import subway.repository.StationRepository;
 import subway.service.LineService;
+import subway.service.SectionService;
 import subway.view.InputView;
 import subway.view.OutputView;
 import subway.view.constant.option.LineOptions;
+import subway.view.constant.option.SectionOptions;
+import subway.view.constant.phrase.LinePhrase;
+import subway.view.constant.phrase.SectionPhrase;
+import subway.view.constant.phrase.StationPhrase;
 
 public class SectionController {
     private static class InstanceHolder {
@@ -15,6 +21,7 @@ public class SectionController {
     private InputView inputView = InputView.getInstance();
     private OutputView outputView = OutputView.getInstance();
     private LineService lineService = LineService.getInstance();
+    private SectionService sectionService = SectionService.getInstance();
 
     private final StationRepository stationRepository = StationRepository.getInstance();
     private final LineRepository lineRepository = LineRepository.getInstance();
@@ -37,13 +44,52 @@ public class SectionController {
     }
 
     private void handleOption(String option) {
-        if (option.equals(LineOptions.REGISTER.getOption())) {
+        if (option.equals(SectionOptions.REGISTER.getOption())) {
+            registerSection();
+        }
+        if (option.equals(SectionOptions.DELETE.getOption())) {
 
         }
-        if (option.equals(LineOptions.DELETE.getOption())) {
+    }
 
+    private void registerSection() {
+        String lineName = Requester.requestStringInput(() ->
+                controlExistLineNameInput(SectionPhrase.REGISTER_LINE_INPUT.get()));
+        String stationName = Requester.requestStringInput(() ->
+                controlExistStationNameInput(SectionPhrase.REGISTER_STATION_INPUT.get()));
+        int order = Requester.requestNumberInput(() ->
+                controlRegisterOrderInput(SectionPhrase.REGISTER_ORDER_INPUT.get(), lineName));
+
+        sectionService.registerSection(lineName, stationName, order);
+        outputView.printInfoPhrase(SectionPhrase.REGISTER_INFO.get());
+    }
+
+    private String controlExistLineNameInput(String phrase) {
+        outputView.printPhrase(phrase);
+        String lineName = inputView.readNonEmptyInput();
+        if (!lineRepository.hasLine(lineName)) {
+            throw new IllegalArgumentException(DomainErrorMessage.NOT_EXIST_Line.get());
         }
-        if (option.equals(LineOptions.FIND_ALL.getOption())) {
+        return lineName;
+    }
+
+    private String controlExistStationNameInput(String phrase) {
+        outputView.printPhrase(phrase);
+        String stationName = inputView.readNonEmptyInput();
+        if (!stationRepository.hasStation(stationName)) {
+            throw new IllegalArgumentException(DomainErrorMessage.NOT_EXIST_STATION.get());
+        }
+        return stationName;
+    }
+
+    private int controlRegisterOrderInput(String phrase, String lineName) {
+        outputView.printPhrase(phrase);
+        int order = inputView.readOrder();
+        if (!lineService.isCorrectOrderForRegister(lineName, order)) {
+            throw new IllegalArgumentException(DomainErrorMessage.INVALID_ORDER.get());
+        }
+        return order;
+    }
 
         }
     }
