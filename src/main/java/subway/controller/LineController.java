24e70a71f4e9e3b9151addcd8f0eb6e4cf3435cf
skydreamer21 <skydreamer210@gmail.com>
2022-12-16 16:07:47 +0900

    fix(LineController): 검증 기능 서비스로 분리

--- src/main/java/subway/controller/LineController.java
+++ src/main/java/subway/controller/LineController.java
@@ -5,6 +5,7 @@ import subway.domain.constant.DomainErrorMessage;
 import subway.repository.LineRepository;
 import subway.repository.StationRepository;
 import subway.service.LineService;
+import subway.service.StationService;
 import subway.view.InputView;
 import subway.view.OutputView;
 import subway.view.constant.option.LineOptions;
@@ -13,6 +14,8 @@ import subway.view.constant.phrase.StationPhrase;
 
 import java.util.List;
 import java.util.Set;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 public class LineController {
     private static class InstanceHolder {
@@ -22,9 +25,7 @@ public class LineController {
     private InputView inputView = InputView.getInstance();
     private OutputView outputView = OutputView.getInstance();
     private LineService lineService = LineService.getInstance();
-
-    private final StationRepository stationRepository = StationRepository.getInstance();
-    private final LineRepository lineRepository = LineRepository.getInstance();
+    private StationService stationService = StationService.getInstance();
 
     private LineController(){}
 
@@ -57,7 +58,10 @@ public class LineController {
 
     private void registerLine() {
         String lineName = Requester.requestStringInput(() ->
-                controlLineNameInput(LinePhrase.REGISTER.get()));
+                controlLineNameInput(
+                        lineService::validateNonExistLineName,
+                        LinePhrase.REGISTER.get()
+                ));
         String startStationName = Requester.requestStringInput(() ->
                 controlStationNameInput(LinePhrase.REGISTER_START_STATION.get()));
         String endStationName = Requester.requestStringInput(() ->
@@ -69,37 +73,26 @@ public class LineController {
 
     private void removeLine() {
         String lineName = Requester.requestStringInput(() ->
-                controlDeleteLineNameInput(LinePhrase.DELETE.get()));
+                controlLineNameInput(
+                        lineService::validateExistLineName,
+                        LinePhrase.DELETE.get()
+                ));
 
         lineService.removeLine(lineName);
         outputView.printInfoPhrase(LinePhrase.DELETE_INFO.get());
     }
 
-    // TODO: 중복 검증 기능 서비스에서 구현 후 repository 의존성 제거
-    private String controlLineNameInput(String phrase) {
+    private String controlLineNameInput(Consumer<String> consumer, String phrase) {
         outputView.printPhrase(phrase);
         String lineName = inputView.readNonEmptyInput();
-        if (lineRepository.hasLine(lineName)) {
-            throw new IllegalArgumentException(DomainErrorMessage.DUPLICATE_LINE.get());
-        }
-        return lineName;
-    }
-
-    private String controlDeleteLineNameInput(String phrase) {
-        outputView.printPhrase(phrase);
-        String lineName = inputView.readNonEmptyInput();
-        if (!lineRepository.hasLine(lineName)) {
-            throw new IllegalArgumentException(DomainErrorMessage.NOT_EXIST_Line.get());
-        }
+        consumer.accept(lineName);
         return lineName;
     }
 
     private String controlStationNameInput(String phrase) {
         outputView.printPhrase(phrase);
         String stationName = inputView.readNonEmptyInput();
-        if (!stationRepository.hasStation(stationName)) {
-            throw new IllegalArgumentException(DomainErrorMessage.NOT_EXIST_STATION.get());
-        }
+        stationService.validateExistStationName(stationName);
         return stationName;
     }
 
